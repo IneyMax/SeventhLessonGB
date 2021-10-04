d348c5c8b6e638af359956d45dc494e6e8bd159a
IneyMax <mr.algree@gmail.com>
2021-10-04 17:38:23 +0300

    HW 7 p.1

---
+++ SeventhLessonGB/SeventhLessonGB/mylib.cpp
@@ -0,0 +1,51 @@
+﻿#include "mylib.h"
+
+#include <array>
+#include <array>
+#include <array>
+#include <array>
+#include <iostream>
+
+
+void array_init(std::array<float, 15> & cur_array)
+{
+    for (auto &element : cur_array)
+    {
+        element = {1.1f};
+    }
+}
+
+void array_print(std::array<float, 15>& cur_array)
+{
+    for (const auto element : cur_array)
+    {
+        std::cout << element << " ";
+    }
+    std::cout << "\n";
+}
+
+void array_check(std::array<float, 15> & cur_array)
+{
+    int check_plus {};
+    int check_minus {};
+    int check_zero {};
+    for (const auto element : cur_array)
+    {
+        if (element > 0)
+        {
+            check_plus ++;
+        }
+        else if (element < 0)
+        {
+            check_minus ++;
+        }
+            else
+                check_zero ++;
+    }
+    std::cout << "More, than zero: " << check_plus << "\n";
+    std::cout << "Less, than zero: " << check_minus << "\n";
+    std::cout << "Zero: " << check_zero << "\n";
+}
+
+
+
---
+++ SeventhLessonGB/SeventhLessonGB/mylib.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <array>
+
+void array_init(std::array<float, 15> & cur_array);
+void array_print(std::array<float, 15>& cur_array);
+void array_check(std::array<float, 15> & cur_array);
+
+
+
+
+
+
