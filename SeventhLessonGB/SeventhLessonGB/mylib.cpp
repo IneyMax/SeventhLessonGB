c0b5314092ab71f7d7d3eab485307cbb506a79ed
IneyMax <mr.algree@gmail.com>
2021-10-05 17:13:31 +0300

    HW 7 p 1,2,4,5.

--- SeventhLessonGB/SeventhLessonGB/mylib.cpp
+++ SeventhLessonGB/SeventhLessonGB/mylib.cpp
@@ -1,51 +1,138 @@
 ﻿#include "mylib.h"
-
-#include <array>
-#include <array>
-#include <array>
-#include <array>
+#include <fstream>
 #include <iostream>
+#include <random>
+#include <set>
 
+#define CHECK(x,y) ((0 <= x) && (x < y)) ? true : false
+#define SWAP_INT(a, b)
 
-void array_init(std::array<float, 15> & cur_array)
-{
-    for (auto &element : cur_array)
+void float_array::array_init(std::array<float, 15> & cur_array)
     {
-        element = {1.1f};
+        for (auto &element : cur_array)
+        {
+            element = {1.1f};
+        }
+    }
+
+void float_array::array_print(std::array<float, 15>& cur_array)
+    {
+        for (const auto element : cur_array)
+        {
+            std::cout << element << " ";
+        }
+        std::cout << "\n";
+    }
+
+void float_array::array_check(std::array<float, 15> & cur_array)
+    {
+        int check_plus {};
+        int check_minus {};
+        int check_zero {};
+        for (const auto element : cur_array)
+        {
+            if (element > 0)
+            {
+                check_plus ++;
+            }
+            else if (element < 0)
+            {
+                check_minus ++;
+            }
+            else
+                check_zero ++;
+        }
+        std::cout << "More, than zero: " << check_plus << "\n";
+        std::cout << "Less, than zero: " << check_minus << "\n";
+        std::cout << "Zero: " << check_zero << "\n";
+    }
+
+
+void define_check()
+{
+    int x {};
+    std::cin >> x;
+    int y {10};
+    if (CHECK(x,y))
+    {
+        std::cout << "True\n";
+    }
+    else
+        std::cout << "False\n";
+}
+
+
+void create_new_empolee()
+{
+    // Динамически объявляем переменную и инициализируем:
+    employee *empolee_ptr = new employee;
+    empolee_ptr->age = 24;
+    empolee_ptr->experience = 3;
+    empolee_ptr->rank = 'B';
+
+    // Вводим имя нового сотрудника:
+    std::cout << "Write the name of the new employee:\n";
+    std::cin >> empolee_ptr->name;
+    std::ofstream new_empolee ((empolee_ptr->name + ".txt"));
+    new_empolee << "Name: " <<  empolee_ptr->name << std::endl;
+    new_empolee << "Age: " <<  empolee_ptr->age << std::endl;
+    new_empolee << "Experiance: " <<  empolee_ptr->experience << std::endl;
+    new_empolee << "Rank: " <<  empolee_ptr->rank << std::endl;
+    new_empolee.close();
+
+    //Выводим размер структуры:
+    std::cout << sizeof(*empolee_ptr) << "\n";
+
+    // Выводим записанные в файл данные:
+    std::ifstream cur_empolee((empolee_ptr->name + ".txt"));
+    if (cur_empolee.is_open())
+    {
+        while (!cur_empolee.eof())
+        {
+            std::string buffer;
+            getline(cur_empolee, buffer);
+            if (buffer != "") // чтобы не выводились пустые строки
+            {
+                std::cout << buffer << "\n";
+            }
+        }
     }
 }
 
-void array_print(std::array<float, 15>& cur_array)
+// Временно для инициализации массива:
+int get_random_number()
 {
-    for (const auto element : cur_array)
+    std::random_device rd;   // non-deterministic generator
+    std::mt19937 gen(rd());  // to seed mersenne twister.
+    std::uniform_int_distribution<> dist(0,127); // distribute results between 1 and 6 inclusive.
+    return dist(gen);
+}
+
+void init_array(std::array<int, ARRAY_SIZE> &my_array)
+{
+    for (auto &element : my_array)
     {
-        std::cout << element << " ";
+        element = get_random_number();
+    }
+}
+
+void print_array(std::array<int, ARRAY_SIZE>& my_array)
+{
+    for (const auto array : my_array)
+    {
+        std::cout << array << " ";
     }
     std::cout << "\n";
 }
 
-void array_check(std::array<float, 15> & cur_array)
+void quick_sort_array (std::array<int, ARRAY_SIZE> &my_array, int size_array)
 {
-    int check_plus {};
-    int check_minus {};
-    int check_zero {};
-    for (const auto element : cur_array)
-    {
-        if (element > 0)
-        {
-            check_plus ++;
-        }
-        else if (element < 0)
-        {
-            check_minus ++;
-        }
-            else
-                check_zero ++;
-    }
-    std::cout << "More, than zero: " << check_plus << "\n";
-    std::cout << "Less, than zero: " << check_minus << "\n";
-    std::cout << "Zero: " << check_zero << "\n";
+    ;
 }
 
 
-
+/*
+std::cout <<"Left index: " << left_index << " Right index: " << right_index << "\n";
+std::cout <<"Left index value: " << my_array [left_index] << " Right index value: " << my_array [right_index] << "\n";
+std::cout <<"Support point: " << support_point << "\n";
+*/
--- SeventhLessonGB/SeventhLessonGB/mylib.h
+++ SeventhLessonGB/SeventhLessonGB/mylib.h
@@ -1,10 +1,37 @@
 ﻿#pragma once
 #include <array>
+#include <string>
+#define ARRAY_SIZE 9
 
-void array_init(std::array<float, 15> & cur_array);
-void array_print(std::array<float, 15>& cur_array);
-void array_check(std::array<float, 15> & cur_array);
+//ДЗ 7 п.1,5:
+namespace float_array
+{
+    void array_init(std::array<float, 15> & cur_array);
+    void array_print(std::array<float, 15>& cur_array);
+    void array_check(std::array<float, 15> & cur_array);
+}
 
+//ДЗ 7 п.2:
+void define_check();
+
+
+//ДЗ 7 п.4:
+#pragma pack(push, 1)
+struct employee
+{
+    int age;
+    int experience;
+    char rank;
+    std::string name;
+};
+#pragma pack(pop)
+
+void create_new_empolee();
+
+//ДЗ 7 п.3:
+void init_array(std::array<int, ARRAY_SIZE> &my_array);
+void print_array(std::array<int, ARRAY_SIZE>& my_array);
+void quick_sort_array (std::array<int, ARRAY_SIZE> &my_array, int size_array);
 
 
 
